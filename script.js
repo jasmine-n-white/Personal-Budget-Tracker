f3c9387586afb3038ab8ac2e5bd483008af594e5
Jasmine White <jasmine@whitepdx.com>
2024-03-23 08:24:29 -0700

    created js file

---
+++ script.js
@@ -0,0 +1,27 @@
+const prompt = require("prompt-sync")({sigint: true});
+
+let dontExit = true;
+
+while(dontExit) {
+    // Write your code below
+
+//arrays for income entries
+//objects for expense entries
+
+//six functions
+
+//add income
+//add expense
+//remove expense
+//view expense statement
+//view summary
+
+//use a loop
+
+//add income
+//add expenses
+//remove expenses
+//view transactions
+//view summary
+//exit
+}
