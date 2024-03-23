1434fa5bd31c3cf796182f4f7b12d5eb12f4bc6a
Jasmine White <jasmine@whitepdx.com>
2024-03-23 09:11:27 -0700

    starting to create functions

--- script.js
+++ script.js
@@ -1,10 +1,46 @@
 const prompt = require("prompt-sync")({sigint: true});
 
+console.log("Welcome to the Personal Budget Tracking Application!");
+console.log("====================================================");
+console.log("Choose an option: ");
+console.log("1. Add income");
+console.log("2. Add expenses");
+console.log("3. Remove expenses");
+console.log("4. View transactions");  //income array and expenses 
+console.log("5. View summary"); //total income, total expenses, balance
+console.log("6. Exit the program");
+console.log("====================================================");
+console.log("\n");
+
 let dontExit = true;
+let income = [];
+let expenses = {};
+//functions
 
+const addIncome = (income) => {
+
+}
+const addExpense = (expenses) => {
+
+}
+const removeExpense = (expenses) => {
+    
+}
+const viewExpenses = (expenses) => {
+    
+}
+const addExpense = (expense) => {
+    
+}
+const addExpense = (expense) => {
+    
+}
 while(dontExit) {
-    // Write your code below
-
+//if option 1
+//add income
+//if option 2
+//remove income
+//
 //arrays for income entries
 //objects for expense entries
 
